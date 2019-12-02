c3556e68d800ceaffeafa25f0a103872b2fec838
cex <cex@nasa.gov>
2019-12-02 18:01:57 +0530

    Initial commit.

---
+++ upload.sh
@@ -0,0 +1,6 @@
+echo "# githubpagestest" >> README.md
+git init
+git add .
+git commit -m "Initial commit."
+git remote add origin https://github.com/curexcode/githubpagestest.git
+git push -u origin master
